e64007f2b99b8ec5c3ed0ed6152c086e35edca73
Miguel Vidal <rivensona@gmail.com>
2017-02-16 08:12:24 -0600

    Yea

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ import * as LoginController from './controller/loginController';
 
 import express = require('express');
 
+
 export function defineRoutes(app: express.Application) {
   app.use('/login', LoginController.router);
 
